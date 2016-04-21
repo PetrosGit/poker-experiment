b94170fce8f14427b9e358983195d5c8f9bf920b
dlivas <dimitris.livas@gmail.com>
2016-04-21 14:56:11 +0300

    update cards

--- src/views/HomeView/index.jsx
+++ src/views/HomeView/index.jsx
@@ -1,30 +1,47 @@
-
-
 import React from 'react';
 import './homeview.css';
 
+import { Card, Hand } from 'components/cards';
 
-import { Card, Hand , BackHand } from 'components/cards';
+const Style = {
+  container: {
+    marginTop: '30px',
+    width: '100%',
+  },
+  title: {
+  },
+  table: {
+    marginTop: '40px',
+    width: '350px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+};
 
-export default class HomeView extends React.Component {
+class HomeView extends React.Component {
   render () {
     return (
-    <div>
-    	<h1 > Five Card Draw Poker </h1>
-    	<div className = 'pokerhand'>
-    		<BackHand/>
-
-
-      		<Hand cards={[
-      	  		{ rank:'Q', suit:'diams' },
-     	 		  { rank:'J', suit:'diams' },
-     		      { rank:'10', suit:'diams' },
-     		   	  { rank:'9', suit:'diams' },
-        		  { rank:'8', suit:'diams' },
-       		 ]} />
-      	</div>
-    </div>
-      
+      <div style={Style.container}>
+        <h1> Five Card Draw Poker </h1>
+        <div style={Style.table}>
+          <Hand cards={[
+            { back: true },
+            { back: true },
+            { back: true },
+            { back: true },
+            { back: true },
+          ]} />
+          <Hand cards={[
+            { rank:'Q', suit:'diams' },
+            { rank:'J', suit:'diams' },
+            { rank:'10', suit:'diams' },
+            { rank:'9', suit:'diams' },
+            { rank:'8', suit:'diams' },
+          ]} />
+        </div>
+      </div>
     );
   }
 }
+
+export default HomeView;
