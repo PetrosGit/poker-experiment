7c639db74b3b2b2f99774c3455435695e2445957
dlivas <dimitris.livas@gmail.com>
2016-04-21 16:15:59 +0300

    update cards

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -5,14 +5,14 @@ import classes from './cards.css';
 export function Card({ rank, suit, back }) {
   if (back === true) {
     return (<div className={Card.className.back} style={Card.style}>*</div>);
-  } else {
-    return (
-      <div className={Card.className.front(rank, suit)} style={Card.style}>
-        <span className={classes.rank}>{rank}</span>
-        <span className={classes.suit}></span>
-      </div>
-    );
   }
+
+  return (
+    <div className={Card.className.front(rank, suit)} style={Card.style}>
+      <span className={classes.rank}>{rank}</span>
+      <span className={classes.suit}></span>
+    </div>
+  );
 }
 
 Card.className =  {
