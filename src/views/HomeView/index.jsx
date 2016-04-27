39313904916bce4d361634873c0f36915ff2f31d
dlivas <dimitris.livas@gmail.com>
2016-04-27 11:51:09 +0300

    one little hint dear petros :)

--- src/views/HomeView/index.jsx
+++ src/views/HomeView/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './homeview.css';
-import {Startbutton} from 'components/buttons';
+import { StartButton } from 'components/buttons';
 import { Card, Hand } from 'components/cards';
-
 
 const Style = {
   container: {
@@ -16,30 +15,83 @@ const Style = {
   },
 };
 
-class HomeView extends React.Component {
+class PlayingView extends React.Component {
   render () {
+    return (
+      <div style={Style.table}>
+        <Hand cards={[
+          { back: true },
+          { back: true },
+          { back: true },
+          { back: true },
+          { back: true },
+        ]} />
+        <Hand cards={[
+          { rank:'Q', suit:'diams' },
+          { rank:'J', suit:'diams' },
+          { rank:'10', suit:'clubs' },
+          { rank:'9', suit:'diams' },
+          { rank:'8', suit:'diams' },
+        ]} />
+      </div>
+    );
+  }
+}
+
+class WelcomeView extends React.Component {
+  render() {
+    return (
+      <div style={Style.table}>
+        Start your new Game!!!
+      </div>
+    );
+  }
+}
+
+class HomeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playingView: false,
+    };
+
+    this.onClick = () => {
+      this.setState({
+        playingView: !this.state.playingView,
+      });
+    };
+  }
+
+  renderStartGameButton() {
+    return (
+      <span>
+        Start Game
+      </span>
+    );
+  }
+
+  renderEndGameButton() {
+    return (
+      <span>
+        End Game
+      </span>
+    );
+  }
+
+  render () {
+    const view = this.state.playingView
+    ? <PlayingView />
+    : <WelcomeView />;
+    const buttonContent = this.state.playingView
+    ? this.renderEndGameButton()
+    : this.renderStartGameButton();
+
     return (
       <div style={Style.container}>
         <h1> Five Card Draw Poker </h1>
-        
-        <div style={Style.table}>
-          <Hand cards={[
-            { back: true },
-            { back: true },
-            { back: true },
-            { back: true },
-            { back: true },
-          ]} />
-          <Hand cards={[
-            { rank:'Q', suit:'diams' },
-            { rank:'J', suit:'diams' },
-            { rank:'10', suit:'clubs' },
-            { rank:'9', suit:'diams' },
-            { rank:'8', suit:'diams' },
-          ]} />
-        </div>
 
-      <Startbutton/>
+        {view}
+        <button onClick={this.onClick}>{buttonContent}</button>
       </div>
     );
   }
