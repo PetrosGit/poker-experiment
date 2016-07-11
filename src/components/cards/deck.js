399124c57c09625566f542655c0d2845841f818c
petrosgit <petrosgkikasgit@gmail.com>
2016-07-11 10:17:54 +0300

    Revert "clean up"
    This reverts commit bd440e0782270e05e663757cdfcbf5319c7bc0f2.

--- src/components/cards/deck.js
+++ src/components/cards/deck.js
@@ -1,18 +1,63 @@
 import _ from 'lodash';
-const Ranks = ['2' , '3' , '4' , '5' , '6' , '7' , '8', '9' , '10' , 'J' , 'Q' , 'K' , 'A'];
-const Suits = ['diams' , 'spades' , 'hearts' , 'clubs'];
 
-let fullDeck = [];
+let fullDeck = [
+  {rank : '2' , suit : 'diams'},
+  {rank : '3' , suit : 'diams'},
+  {rank : '4' , suit : 'diams'},
+  {rank : '5' , suit : 'diams'},
+  {rank : '6' , suit : 'diams'},
+  {rank : '7' , suit : 'diams'},
+  {rank : '8' , suit : 'diams'},
+  {rank : '9' , suit : 'diams'},
+  {rank : '10' , suit : 'diams'},
+  {rank : 'J' , suit : 'diams'},
+  {rank : 'Q' , suit : 'diams'},
+  {rank : 'K' , suit : 'diams'},
+  {rank : 'A' , suit : 'diams'},
+  {rank : '2' , suit : 'spades'},
+  {rank : '3' , suit : 'spades'},
+  {rank : '4' , suit : 'spades'},
+  {rank : '5' , suit : 'spades'},
+  {rank : '6' , suit : 'spades'},
+  {rank : '7' , suit : 'spades'},
+  {rank : '8' , suit : 'spades'},
+  {rank : '9' , suit : 'spades'},
+  {rank : '10' , suit : 'spades'},
+  {rank : 'J' , suit : 'spades'},
+  {rank : 'Q' , suit : 'spades'},
+  {rank : 'K' , suit : 'spades'},
+  {rank : 'A' , suit : 'spades'},
+  {rank : '2' , suit : 'hearts'},
+  {rank : '3' , suit : 'hearts'},
+  {rank : '4' , suit : 'hearts'},
+  {rank : '5' , suit : 'hearts'},
+  {rank : '6' , suit : 'hearts'},
+  {rank : '7' , suit : 'hearts'},
+  {rank : '8' , suit : 'hearts'},
+  {rank : '9' , suit : 'hearts'},
+  {rank : '10' , suit : 'hearts'},
+  {rank : 'J' , suit : 'hearts'},
+  {rank : 'Q' , suit : 'hearts'},
+  {rank : 'K' , suit : 'hearts'},
+  {rank : 'A' , suit : 'hearts'},
+  {rank : '2' , suit : 'clubs'},
+  {rank : '3' , suit : 'clubs'},
+  {rank : '4' , suit : 'clubs'},
+  {rank : '5' , suit : 'clubs'},
+  {rank : '6' , suit : 'clubs'},
+  {rank : '7' , suit : 'clubs'},
+  {rank : '8' , suit : 'clubs'},
+  {rank : '9' , suit : 'clubs'},
+  {rank : '10' , suit : 'clubs'},
+  {rank : 'J' , suit : 'clubs'},
+  {rank : 'Q' , suit : 'clubs'},
+  {rank : 'K' , suit : 'clubs'},
+  {rank : 'A' , suit : 'clubs'},
+];
 
-for (const [ weight, rank ] of Ranks.entries()) {
-  for (const suit of Suits) {
-    fullDeck.push({ suit, rank, weight: String.fromCharCode(weight + 65) });
-  };
- };
-  
 export function Deck() {
-  let newDeck = { ...fullDeck };
-  return _.shuffle(newDeck);
+	let newDeck = { ...fullDeck };
+	return _.shuffle(newDeck);
 };
 
 export function deckGetHand(deck,count) {
