c0912c770e12a3bb2e5e33a881b5c571df9a1bd2
Menelaos Zetas <menelaoszetas@gmail.com>
2016-09-29 17:05:40 +0300

    CSS
    Css sto aboutUs alla gia na mpei h eikona thelei na doume kati apo to
    davezouko

--- src/routes/AboutUs/Components/AboutUs.js
+++ src/routes/AboutUs/Components/AboutUs.js
@@ -1,11 +1,39 @@
 
 import React from 'react';
 
-const AboutUs = () => (
-      <div>
-        <h3>Poker Team</h3>
-        <p>Menelaos Christos Petros</p>
+let AboutUs = () => (
+      <div style={Style.container}>
+        <h3 style={Style.header}>Poker Team</h3>
+        <h3 style={Style.contributorTitle}>Supervisor - King</h3>
+        <p style={Style.contributors}>Δημήτρης Λιβάς</p>
+        <h3 style={Style.contributorTitle}>Programmers - Slaves</h3>
+        <p style={Style.contributors}>Χρήστος Τσόπελας</p>
+        <p style={Style.contributors}>Πέτρος ΕΠΙΘΕΤΟ !!!!</p>
+        <p style={Style.contributors}>Μενέλαος Ζέτας</p>
       </div>
   );
+
+const Style = {
+  container: {
+  },
+  header: {
+    textAlign: 'center',
+    fontFamily: 'Arial',
+    fontSize: 30,
+    margin: 30,
+  },
+  contributorTitle: {
+    textAlign: 'center',
+    fontFamily: 'Arial',
+    fontSize: 22,
+    color: 'black',
+  },
+  contributors: {
+    textAlign: 'center',
+    fontFamily: 'Arial',
+    fontSize: 20,
+    color: 'red',
+  },
+};
 
 export default AboutUs;
